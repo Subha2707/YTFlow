edf3b8f1e00f5e5efcfead33372576a6468fffcd
Subha2707 <deysubahdip66@gmail.com>
2026-05-03 19:25:04 +0530

    Production config for deployment

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://ytflow.netlify.app/' 
+    'https://ytflow.netlify.app' 
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
